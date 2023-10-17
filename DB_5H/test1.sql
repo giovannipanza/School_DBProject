e8a571ab5f6d2569dc3abb1c5301360fbb61c336
panzagiovanni <panzagiovanni@gmail.com>
2023-10-17 12:07:47 +0200

    new update

--- DB_5H/test1.sql
+++ DB_5H/test1.sql
@@ -1,8 +1,19 @@
+-- prima soluzione
 DROP DATABASE IF EXISTS prova;
 
 create DATABASE prova;
 
--- use prova
 CREATE table prova.tab1 (
+  campo1 int
+);
+
+-- seconda soluzione
+DROP DATABASE IF EXISTS prova;
+
+create DATABASE prova;
+
+use prova;
+
+CREATE table tab1 (
   campo1 int
 );
